04a9b6afd73b47c1f1a2297cd0f5fa669356dba1
Bhaumik Patel <bhaumik7230@gmail.com>
2020-12-05 23:16:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const tf = require('@tensorflow/tfjs');
+
+const w0 = tf.scalar(Math.random()).variable();
+const w1 = tf.scalar(Math.random()).variable();
+const ones = tf.ones([1,1]);
+
+const xs = tf.tensor1d([5.1, 4.7, 6.4, 7.0]);
+const ys = tf.tensor1d([1, 1, 0, 0]);
+
+// xi=>1/(1+Math.exp(-(P[0]+(P[1]*xi))))
+const f = x => ones.div(ones.sum(tf.exp((w0.sum(w1.mul(x))).neg)))
+
+const loss = (pred, label) => tf.mean(tf.square(tf.sub(pred, label)));
+
+console.log(f(xs))
+const optimizer = tf.train.sgd(0.01);
+
+// Train the model.
+for (let i = 0; i < 10; i++) {
+   optimizer.minimize(() => loss(f(xs), ys));
+}
+
+// Make predictions.
+const preds = f(xs).dataSync();
+preds.forEach((pred, i) => {
+   console.log(`x: ${i}, pred: ${pred}`);
+});
