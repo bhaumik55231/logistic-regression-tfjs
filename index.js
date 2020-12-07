14c8cd701e4fdd720a159786285bf8d0a089b16c
Bhaumik Patel <bhaumik55231@gmail.com>
2020-12-07 19:36:57 +0000

    Minor update

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const tf = require('@tensorflow/tfjs');
 
 const w0 = tf.scalar(Math.random()).variable();
 const w1 = tf.scalar(Math.random()).variable();
-const ones = tf.ones([1,1]);
 
 const xs = tf.tensor1d([5.1, 4.7, 6.4, 7.0]);
 const ys = tf.tensor1d([1, 1, 0, 0]);
 
-const f = x => ones.div(ones.add(((w0.add(w1.mul(x))).neg()).exp()))
+const f = x => tf.div(1, tf.add(1, ((w0.add(w1.mul(x))).neg()).exp()))
 
-const loss = (pred, label) => tf.mean(tf.square(tf.sub(pred, label)));
+const loss = (pred, label) => ((pred.sub(label)).square()).mean();
 
 const optimizer = tf.train.sgd(0.01);
 
